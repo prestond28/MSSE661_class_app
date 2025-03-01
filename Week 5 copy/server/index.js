84c81c6b48b4e41e38020f6921b4e061ea406c3a
prestond28 <pdavis2897@gmail.com>
2025-02-28 21:06:39 -0600

    finished week 5 content

---
+++ Week 5 copy/server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.use(express.static("public"));
+
+app.use("/css", express.static(__dirname + "/public/css"));
+app.use("/js", express.static(__dirname + "/public/src"));
+
+app.listen(port, function() {
+    console.log("Server started at http://localhost:%s", port);
+});
